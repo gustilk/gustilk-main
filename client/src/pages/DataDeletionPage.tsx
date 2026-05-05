67f186517697983330680249e02b55341ffa215c
gustilk <gustilk@users.noreply.github.com>
2026-05-05 16:40:27 -0500

    feat: add data deletion instructions page at /data-deletion

---
+++ client/src/pages/DataDeletionPage.tsx
@@ -0,0 +1,11 @@
+import PolicyPage from "./PolicyPage";
+
+export default function DataDeletionPage() {
+  return (
+    <PolicyPage
+      titleKey="dataDeletion.pageTitle"
+      introKey="dataDeletion.intro"
+      sectionsKey="dataDeletion.sections"
+    />
+  );
+}
